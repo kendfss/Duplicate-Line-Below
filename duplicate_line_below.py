2f10824ae4a407cb539362c2f1de8c9d28784651
kendfss <kennethsantanasabalo@gmail.com>
2021-08-12 17:54:50 +0100

    first

--- duplicate_line_below.py
+++ duplicate_line_below.py
@@ -2,7 +2,7 @@ import sublime
 import sublime_plugin
 
 
-class DuplicateBelowCommand(sublime_plugin.TextCommand):
+class DuplicateLineBelowCommand(sublime_plugin.TextCommand):
     def run(self, edit):
         self.view.run_command('duplicate_line')
         self.view.run_command('swap_line_up')
